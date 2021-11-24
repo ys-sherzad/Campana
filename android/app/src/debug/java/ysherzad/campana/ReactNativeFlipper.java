32e33a16994ecdb93ef91de64f8ae6f7e6064ec7
YasirSherzad <ys.sherzad@gmail.com>
2021-11-25 00:21:25 +0430

    rename app to campana

--- android/app/src/debug/java/ysherzad/campana/ReactNativeFlipper.java
+++ android/app/src/debug/java/ysherzad/campana/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package ysherzad.rn.template;
+package ysherzad.campana;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
